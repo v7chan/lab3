4be22bb192174b6264abf41c358b1fe437cab00e
Vincent Chan <v7chan@gmail.com>
2015-01-22 12:59:24 -0800

    Initial update for lab3

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,42 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+    $(".jumbotron h1").text("Javascript is connected");
+		$(this).text("EXPERIENCE!");
+    $(".jumbotron p").toggleClass("active");
 	});
 
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+	$('a.thumbnail').click(projectClick);
+
+  $('#submitBtn').click(updateProject);
+}
+
+function projectClick(e) {
+  e.preventDefault();
+  // $(this).css("background-color", "#7fff00");
+
+  var projectTitle = $(this).find("p").text();
+  var jumbotronHeader = $(".jumbotron h1");
+  jumbotronHeader.text(projectTitle);
+
+  var containingProject = $(this).closest(".project");
+  var description = $(containingProject).find(".project-description");
+
+  if (description.length == 0) {
+    $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+  }
+  else {
+    $(".project-description").hide();
+  }
+}
+
+function updateProject(e) {
+  var projectID = $('#project').val();
+
+  $(projectID).animate({
+    width: $('#width').val()
+  });
+
+  var newText = $('#description').val();
+  $(projectID + " .project-description").text(newText);
 }
