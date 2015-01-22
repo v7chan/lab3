3bb38af8f90b938c9fc082c527d64cfcc7fbd599
Vincent Chan <v7chan@gmail.com>
2015-01-22 13:24:21 -0800

    Add stretch goal

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -15,29 +15,47 @@ function initializePage() {
     $(".jumbotron p").toggleClass("active");
 	});
 
-	$('a.thumbnail').click(projectClick);
+	$('a').click(projectClick);
 
   $('#submitBtn').click(updateProject);
+  $(".project img").hide();
+  $(".project-description").hide();
 }
 
 function projectClick(e) {
   e.preventDefault();
-  // $(this).css("background-color", "#7fff00");
+  var containingProject = $(this).parent();
 
-  var projectTitle = $(this).find("p").text();
-  var jumbotronHeader = $(".jumbotron h1");
-  jumbotronHeader.text(projectTitle);
-
-  var containingProject = $(this).closest(".project");
-  var description = $(containingProject).find(".project-description");
-
-  if (description.length == 0) {
-    $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+  if ($(containingProject).hasClass("shown")) {
+    $(containingProject).removeClass("shown");
+    $(containingProject).find("img").hide();
+    $(containingProject).find('.project-description').hide();
   }
   else {
-    $(".project-description").hide();
+    $(containingProject).addClass("shown");
+    $(containingProject).find("img").show();
+    $(containingProject).find('.project-description').show();
   }
 }
+
+// function projectClick(e) {
+//   e.preventDefault();
+//   // $(this).css("background-color", "#7fff00");
+
+//   var projectTitle = $(this).find("p").text();
+//   var jumbotronHeader = $(".jumbotron h1");
+//   jumbotronHeader.text(projectTitle);
+
+//   var containingProject = $(this).closest(".project");
+//   var description = $(containingProject).find(".project-description");
+
+//   if (description.length == 0) {
+//     $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+//   }
+//   else {
+//     $(".project-description").hide();
+//   }
+// }
 
 function updateProject(e) {
   var projectID = $('#project').val();
